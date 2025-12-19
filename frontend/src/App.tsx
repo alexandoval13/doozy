47c5c3ff657d083d9be51f127705ad5165611fa7
Alexandra Sandoval <alexandoval13@gmail.com>
2025-12-18 17:55:36 -0800

    Integrate frontend with Vite React

---
+++ frontend/src/App.tsx
@@ -0,0 +1,221 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+type TaskCategory = 'today' | 'tomorrow';
+
+interface Task {
+  id: number;
+  title: string;
+  hour: number;
+  urgency: number;
+  effort: number;
+  category: TaskCategory;
+  priority: number;
+}
+
+export default function App() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [listView, setListView] = useState<TaskCategory>('today');
+
+  const [title, setTitle] = useState<string>('');
+  const [hour, setHour] = useState<string>('');
+  const [urgency, setUrgency] = useState<string>('');
+  const [effort, setEffort] = useState<string>('');
+
+  async function loadTasks() {
+    try {
+      const res = await fetch('http://localhost:3000/tasks');
+      const data = await res.json();
+
+      if (!Array.isArray(data)) {
+        console.error('Expected array data response. Actual response: ', {
+          data,
+        });
+        return;
+      }
+
+      setTasks(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async function handleAddTask(e: React.FormEvent) {
+    e.preventDefault();
+
+    await fetch('http://localhost:3000/tasks', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        title,
+        hour: Number(hour),
+        urgency: Number(urgency),
+        effort: Number(effort),
+      }),
+    });
+
+    setTitle('');
+    setHour('');
+    setUrgency('');
+    setEffort('');
+
+    loadTasks();
+  }
+
+  async function handleDeleteTask(id: number | string) {
+    await fetch(`http://localhost:3000/tasks/${id}`, {
+      method: 'DELETE',
+    });
+
+    loadTasks();
+  }
+
+  useEffect(() => {
+    loadTasks();
+  }, []);
+
+  return (
+    <>
+      <div style={{ maxWidth: '600px', minWidth: '506px', margin: '24px' }}>
+        <h1>Tasks</h1>
+        <form
+          id="task-form"
+          style={{ display: 'flex', flexDirection: 'column', rowGap: '4px' }}
+          onSubmit={(e) => {
+            handleAddTask(e);
+          }}
+        >
+          <input
+            onChange={(e) => setTitle(e.target.value)}
+            placeholder="Title"
+            id="title"
+            value={title}
+          />
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+            }}
+          >
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'row',
+                columnGap: '12px',
+              }}
+            >
+              <div style={{ display: 'flex', flexDirection: 'column' }}>
+                <input
+                  onChange={(e) => setHour(e.target.value)}
+                  type="number"
+                  placeholder="0-23"
+                  id="hour"
+                  value={hour}
+                />
+                <label htmlFor="hour">Hour</label>
+              </div>
+              <div style={{ display: 'flex', flexDirection: 'column' }}>
+                <input
+                  onChange={(e) => setUrgency(e.target.value)}
+                  type="number"
+                  placeholder="1-5"
+                  id="urgency"
+                  value={urgency}
+                />
+                <label htmlFor="urgency">Urgency</label>
+              </div>
+              <div style={{ display: 'flex', flexDirection: 'column' }}>
+                <input
+                  onChange={(e) => setEffort(e.target.value)}
+                  type="number"
+                  placeholder="1-5"
+                  id="effort"
+                  value={effort}
+                />
+                <label htmlFor="effort">Effort</label>
+              </div>
+            </div>
+            <div>
+              <button type="submit">Add</button>
+            </div>
+          </div>
+        </form>
+
+        <br />
+
+        <form id="list-view-form">
+          <input
+            onChange={(e) => {
+              const value = e.target.value as TaskCategory;
+              return setListView(value);
+            }}
+            id="radio-select-today"
+            type="radio"
+            name="list-view"
+            value={'today'}
+            checked={listView === 'today'}
+          />
+          <label htmlFor="radio-select-today">Today</label>
+
+          <input
+            onChange={(e) => {
+              const value = e.target.value as TaskCategory;
+              return setListView(value);
+            }}
+            id="radio-select-tomorrow"
+            type="radio"
+            name="list-view"
+            value="tomorrow"
+            checked={listView === 'tomorrow'}
+          />
+          <label htmlFor="radio-select-tomorrow">Tomorrow</label>
+        </form>
+
+        <div style={{ border: '2px solid gray' }}>
+          {listView === 'today' ? (
+            <ul id="task-list-today" style={{ padding: '24px' }}>
+              {tasks
+                .filter((t) => t.category === 'today')
+                .map((t) => (
+                  <li key={`li::today::${t.id}`}>
+                    <div
+                      style={{
+                        display: 'flex',
+                        justifyContent: 'space-between',
+                      }}
+                    >
+                      <p>{t.title}</p>
+                      <button onClick={() => handleDeleteTask(t.id)}>
+                        DELETE
+                      </button>
+                    </div>
+                  </li>
+                ))}
+            </ul>
+          ) : (
+            <ul id="task-list-tomorrow" style={{ padding: '24px' }}>
+              {tasks
+                .filter((t) => t.category === 'tomorrow')
+                .map((t) => (
+                  <li key={`li::tomorrow::${t.id}`}>
+                    <div
+                      style={{
+                        display: 'flex',
+                        justifyContent: 'space-between',
+                      }}
+                    >
+                      <p>{t.title}</p>
+                      <button onClick={() => handleDeleteTask(t.id)}>
+                        DELETE
+                      </button>
+                    </div>
+                  </li>
+                ))}
+            </ul>
+          )}
+        </div>
+      </div>
+    </>
+  );
+}
